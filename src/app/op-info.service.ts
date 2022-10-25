7672155fae73f355da4a4469fb61b15121125b17
Matthew-Andric <matthew@sleepy.day>
2022-10-25 18:57:49 +1100

    implemented grabbing appropriate character data added font added json files

--- src/app/op-info.service.ts
+++ src/app/op-info.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import operatorList from '../assets/json/character_table.json';
+import operatorPatchList from '../assets/json/char_patch_table.json';
+import operatorMeta from '../assets/json/char_meta_table.json';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +13,69 @@ export class OpInfoService {
     return operatorList;
   }
 
-  getOperatorByName(name: string): object {
-    let op = {};
-    (Object.keys(operatorList) as (keyof typeof operatorList)[]).forEach((key, index) => {
+  getOperatorDetailsFullByName(name: string): object {
+    let id = "";
+    let info = {};
+    let altClass = {};
+    let altOps: object[] = [];
+
+    (Object.keys(operatorList) as (keyof typeof operatorList)[]).forEach((key) => {
       if (operatorList[key]["name"] === name) {
-        op = operatorList[key];
-        return
+        id = key;
+        info = operatorList[key];
+        return;
       }
     })
 
-    return op;
+    if (id === "") {
+      return {};
+    }
+
+    let metaChars = operatorMeta["spCharGroups"];
+    let altIds: string[] = [];
+
+    (Object.keys(metaChars) as (keyof typeof metaChars)[]).forEach((key) => {
+      if (metaChars[key].includes(id)) {
+        metaChars[key].forEach((v) => {
+          if (v !== id) {
+            altIds.push(v);
+            console.log("alternate found:", v);
+          }
+        })
+      }
+    })
+
+    for (const s of altIds) {
+      (Object.keys(operatorList) as (keyof typeof operatorList)[]).forEach((key, index) => {
+        if (key === s && s !== id) {
+          altOps.push(Object.values(operatorList)[index])
+        }
+      })
+    }
+
+    let patchChars = operatorPatchList["patchChars"];
+    (Object.keys(patchChars) as (keyof typeof patchChars)[]).forEach((key) => {
+      if (patchChars[key]["name"] === name) {
+        altClass = { id: key, info: patchChars[key] };
+        return;
+      }
+    })
+
+    return {id: id, info: info, altClass: altClass, altOps: altOps};
+  }
+
+  getOperatorById(id: string): object {
+    let opId = "";
+    let info = {};
+
+    (Object.keys(operatorList) as (keyof typeof operatorList)[]).forEach((key) => {
+      if (key === id) {
+        opId = key;
+        info = operatorList[key]
+        return;
+      }
+    })
+
+    return { id: opId, info: info };
   }
 }
--- src/app/operator/operator.component.ts
+++ src/app/operator/operator.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { OpInfoService } from '../op-info.service';
 
 @Component({
   selector: 'app-operator',
   templateUrl: './operator.component.html',
-  styleUrls: ['./operator.component.scss']
+  styleUrls: ['./operator.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class OperatorComponent implements OnInit {
-  operator: object;
+  operator: object
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private opService: OpInfoService,
+    public sanitizer: DomSanitizer,
+  ) { }
 
   ngOnInit(): void {
-    this.operator
+    this.route.paramMap
+    this.route.params.subscribe(params => {
+      let name = params['opName']
+      console.log("ee:", name)
+      this.operator = this.opService.getOperatorDetailsFullByName(name)
+      console.log(this.operator)
+    })
+  }
+
+  parseDescription(desc: string) {
+    const r = new RegExp('(<@ba\.kw>)(.*)(<\/>)')
+    desc.replace(r, "<span class='highlight'>$2<\/span>")
+    console.log(desc)
+    return desc.replace(r, "<span class='highlight'>$2<\/span>")
   }
 
 }
